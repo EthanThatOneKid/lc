93757d39cb9efb32dd430c4bbb88857197c2b30f
Ethan Davidson <31261035+EthanThatOneKid@users.noreply.github.com>
2024-03-26 17:16:33 -0700

    Create subarray_product_less_than_k.ts

---
+++ 713/subarray_product_less_than_k.ts
@@ -0,0 +1,22 @@
+export function numSubarrayProductLessThanK(nums: number[], k: number): number {
+  if (k <= 1) {
+    return 0;
+  }
+
+  let count = 0;
+  let product = 1;
+  let left = 0;
+
+  for (let right = 0; right < nums.length; right++) {
+    product *= nums[right];
+
+    while (product >= k) {
+      product /= nums[left];
+      left++;
+    }
+
+    count += right - left + 1;
+  }
+
+  return count;
+}
