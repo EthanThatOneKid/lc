e3107fe2721179e247c0c8060642adf9fa4c6502
EthanThatOneKid <31261035+EthanThatOneKid@users.noreply.github.com>
2024-06-02 17:57:24 -0700

    Update append_characters_to_string_to_make_subsequence.ts

--- 2486/append_characters_to_string_to_make_subsequence.ts
+++ 2486/append_characters_to_string_to_make_subsequence.ts
@@ -1,14 +1,18 @@
 export function appendCharacters(s: string, t: string): number {
-  let j = 0;
-  for (let i = 0; i < s.length; i++) {
-    if (s[i] === t[j]) {
-      j++;
-    }
-
-    if (j === t.length) {
-      return 0;
-    }
-  }
-
-  return t.length - j;
+  return t.length - s.split("").reduce((j, c) => c === t[j] ? j + 1 : j, 0);
 }
+
+// function appendCharacters(s: string, t: string): number {
+//   let j = 0;
+//   for (let i = 0; i < s.length; i++) {
+//     if (s[i] === t[j]) {
+//       j++;
+//     }
+
+//     if (j === t.length) {
+//       return 0;
+//     }
+//   }
+
+//   return t.length - j;
+// }
