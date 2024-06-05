ddc142b32f0a926eae5a833faafd249ff0ffb0b5
EthanThatOneKid <31261035+EthanThatOneKid@users.noreply.github.com>
2024-06-04 18:37:16 -0700

    Update longest_palindrome.ts

--- 409/longest_palindrome.ts
+++ 409/longest_palindrome.ts
@@ -1,16 +1,17 @@
 export function longestPalindrome(s: string): number {
-  const frequencies = Array.from(
-    s.split("").reduce(
-      (map, c) => map.set(c, (map.get(c) ?? 0) + 1),
-      new Map(),
-    ).values(),
+  return (({ length, oddness }) => length + oddness)(
+    Array.from(
+      s.split("").reduce(
+        (map, c) => map.set(c, (map.get(c) ?? 0) + 1),
+        new Map(),
+      ).values(),
+    )
+      .reduce(
+        ({ length, oddness }, frequency) => ({
+          length: length + Math.floor(frequency * 0.5) * 2,
+          oddness: oddness || frequency % 2,
+        }),
+        { length: 0, oddness: 0 },
+      ),
   );
-  const { length, isOdd } = frequencies.reduce(
-    ({ length, isOdd }, frequency) => ({
-      length: length + Math.floor(frequency * 0.5) * 2,
-      isOdd: isOdd || frequency % 2,
-    }),
-    { length: 0, isOdd: 0 },
-  );
-  return length + isOdd;
 }
