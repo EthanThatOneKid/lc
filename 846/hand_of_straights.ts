c97c332a38a8748d1d71d35b25018602936830a2
Ethan Davidson <31261035+EthanThatOneKid@users.noreply.github.com>
2024-06-07 11:27:56 -0700

    Update hand_of_straights.ts
    road to one line

--- 846/hand_of_straights.ts
+++ 846/hand_of_straights.ts
@@ -1,31 +1,35 @@
 export function isNStraightHand(hand: number[], groupSize: number): boolean {
-  if (hand.length % groupSize !== 0) {
-    return false;
-  }
+  return hand.length % groupSize !== 0 ? false : (function checkGroup(
+    frequencyMap: Map<number, number>,
+    sortedHand: number[],
+  ): boolean {
+    return sortedHand.length === 0 ? true : (() => {
+      const first = sortedHand[sortedHand.length - 1];
+      return (
+          Array.from({ length: groupSize }, (_, i) => i + first).every((i) =>
+            !frequencyMap.has(i) ? false : (() => {
+              const frequency = frequencyMap.get(i)! - 1;
+              frequencyMap.set(i, frequency);
+              if (frequency === 0) {
+                if (i !== sortedHand[sortedHand.length - 1]) {
+                  return false;
+                }
 
-  const frequencyMap = hand.reduce(
-    (map, curr) => map.set(curr, (map.get(curr) ?? 0) + 1),
-    new Map<number, number>(),
+                sortedHand.pop();
+              }
+
+              return true;
+            })()
+          )
+        )
+        ? checkGroup(frequencyMap, sortedHand)
+        : false;
+    })();
+  })(
+    hand.reduce(
+      (map, curr) => map.set(curr, (map.get(curr) ?? 0) + 1),
+      new Map<number, number>(),
+    ),
+    Array.from(new Set(hand)).sort((a, b) => b - a),
   );
-  const sortedHand = Array.from(new Set(hand)).sort((a, b) => b - a);
-  while (sortedHand.length > 0) {
-    const first = sortedHand[sortedHand.length - 1];
-    for (let i = first; i < first + groupSize; i++) {
-      if (!frequencyMap.has(i)) {
-        return false;
-      }
-
-      const frequency = frequencyMap.get(i)! - 1;
-      frequencyMap.set(i, frequency);
-      if (frequency === 0) {
-        if (i !== sortedHand[sortedHand.length - 1]) {
-          return false;
-        }
-
-        sortedHand.pop();
-      }
-    }
-  }
-
-  return true;
 }
