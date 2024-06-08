d5d82894f64e69101211740dfe78582908aef63c
Ethan Davidson <31261035+EthanThatOneKid@users.noreply.github.com>
2024-06-07 22:58:42 -0700

    Update continuous_subarray_sum.ts
    one line

--- 523/continuous_subarray_sum.ts
+++ 523/continuous_subarray_sum.ts
@@ -1,21 +1,11 @@
 function checkSubarraySum(nums: number[], k: number): boolean {
-  if (nums.length < 2) {
-    return false;
-  }
-
-  let j = 0;
-  const sums = new Map<number, number>([[0, -1]]);
-  return nums.some((num, i) => {
-    j = (j + num) % k;
-    if (!sums.has(j)) {
-      sums.set(j, i);
-      return false;
-    }
-
-    if (i - sums.get(j)! > 1) {
-      return true;
-    }
-
-    return false;
-  });
+  return nums.reduce(
+    ({ j, sums, found }, num, i) => ({
+      j: (j + num) % k,
+      sums: sums.has((j + num) % k) ? sums : sums.set((j + num) % k, i),
+      found: found ||
+        (sums.has((j + num) % k) && i - sums.get((j + num) % k)! > 1),
+    }),
+    { j: 0, sums: new Map<number, number>([[0, -1]]), found: false },
+  ).found;
 }
