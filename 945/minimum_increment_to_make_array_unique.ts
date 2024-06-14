62e0801a6f07fe2ddbaa47f4d29c7a25351ec436
Ethan Davidson <31261035+EthanThatOneKid@users.noreply.github.com>
2024-06-13 18:08:40 -0700

    Update minimum_increment_to_make_array_unique.ts

--- 945/minimum_increment_to_make_array_unique.ts
+++ 945/minimum_increment_to_make_array_unique.ts
@@ -1,13 +1,35 @@
-export function minIncrementForUnique(nums: number[]): number {
-  let total = 0;
-  nums.sort((a, b) => a - b);
-  for (let i = 1; i < nums.length; i++) {
-    if (nums[i] <= nums[i - 1]) {
-      const nextValue = nums[i - 1] + 1;
-      total += nextValue - nums[i];
-      nums[i] = nextValue;
-    }
-  }
+// Polyfill for Array.prototype.with method.s
+Array.prototype.with = function <T>(this: T[], i: number, value: T): T[] {
+  this[i] = value;
+  return this;
+};
 
-  return total;
+// Polyfill for Array.prototype.toSorted method.
+Array.prototype.toSorted = function <T>(
+  this: T[],
+  compareFn: (a: T, b: T) => number,
+): T[] {
+  return this.slice().sort(compareFn);
+};
+
+declare global {
+  interface Array<T> {
+    with(i: number, value: T): T[];
+    toSorted(compareFn: (a: T, b: T) => number): T[];
+  }
+}
+
+function minIncrementForUnique(nums: number[]): number {
+  return Array.from({ length: nums.length - 1 })
+    .reduce<{ total: number; nums: number[] }>(
+      ({ total, nums }, _, i) => (nums[i + 1] > nums[i] ? { total, nums } : {
+        total: total + nums[i] - nums[i + 1] + 1,
+        nums: nums.with(i + 1, nums[i] + 1),
+      }),
+      {
+        total: 0,
+        nums: nums.toSorted((a, b) => a - b),
+      },
+    )
+    .total;
 }
