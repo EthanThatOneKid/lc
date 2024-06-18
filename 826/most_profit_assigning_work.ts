9bcaf5ee9172011ce4aa35a13e0522ecba5475bc
Ethan Davidson <31261035+EthanThatOneKid@users.noreply.github.com>
2024-06-17 21:39:40 -0700

    Update most_profit_assigning_work.ts

--- 826/most_profit_assigning_work.ts
+++ 826/most_profit_assigning_work.ts
@@ -2,20 +2,20 @@ function maxProfitAssignment(
   difficulty: number[],
   profit: number[],
   worker: number[],
+  jobs = difficulty.map((d, i) => [d, profit[i]]).sort(([a], [b]) => a - b),
 ): number {
-  const jobs = difficulty.map((d, i) => [d, profit[i]]);
-  jobs.sort((a, b) => a[0] - b[0]);
-  worker.sort((a, b) => a - b);
-  let maxProfit = 0;
-  let i = 0;
-  let best = 0;
-  for (const ability of worker) {
-    while (i < jobs.length && jobs[i][0] <= ability) {
-      best = Math.max(best, jobs[i][1]);
-      i++;
-    }
-    maxProfit += best;
-  }
-
-  return maxProfit;
+  return worker.sort((a, b) => a - b).reduce(
+    ({ best, maxProfit, i }, ability) =>
+      (function find(
+        ability: number,
+        maxProfit = 0,
+        best = 0,
+        i = 0,
+      ): { best: number; i: number; maxProfit: number } {
+        return (i >= jobs.length || jobs[i][0] > ability)
+          ? { best, i, maxProfit: maxProfit + best }
+          : find(ability, maxProfit, Math.max(best, jobs[i][1]), i + 1);
+      })(ability, maxProfit, best, i),
+    { best: 0, maxProfit: 0, i: 0 },
+  ).maxProfit;
 }
