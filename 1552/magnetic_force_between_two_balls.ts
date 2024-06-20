37f9dc67f8aa91687ad0193defd3f4ef7e20c940
Ethan Davidson <31261035+EthanThatOneKid@users.noreply.github.com>
2024-06-19 18:04:51 -0700

    Update magnetic_force_between_two_balls.ts

--- 1552/magnetic_force_between_two_balls.ts
+++ 1552/magnetic_force_between_two_balls.ts
@@ -6,27 +6,16 @@ function maxDistance(
   hi = sortedPosition[sortedPosition.length - 1] - sortedPosition[0],
   mid = ~~((lo + hi) * 0.5),
 ): number {
-  if (lo >= hi) {
-    return lo - 1;
-  }
-
-  return check(sortedPosition, mid, m)
+  return lo > hi ? hi : sortedPosition.reduce(
+      (
+        { count, pre },
+        cur,
+        i,
+      ) => (i !== 0 && cur - pre >= mid
+        ? { count: count + 1, pre: cur }
+        : { count, pre }),
+      { count: 1, pre: sortedPosition[0] },
+    ).count >= m
     ? maxDistance(position, m, sortedPosition, mid + 1, hi)
-    : maxDistance(position, m, sortedPosition, lo, mid);
-}
-
-/**
- * check checks if the balls can be placed with a distance of mid.
- */
-function check(position: number[], mid: number, m: number): boolean {
-  let count = 1;
-  let pre = position[0];
-  for (let i = 1; i < position.length; i++) {
-    if (position[i] - pre >= mid) {
-      count++;
-      pre = position[i];
-    }
-  }
-
-  return count >= m;
+    : maxDistance(position, m, sortedPosition, lo, mid - 1);
 }
