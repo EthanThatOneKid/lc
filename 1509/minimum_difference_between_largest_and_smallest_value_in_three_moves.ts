c017489ac11b4fc38b12cc8f565299e45098690e
EthanThatOneKid <31261035+EthanThatOneKid@users.noreply.github.com>
2024-07-03 09:46:09 -0700

    Create minimum_difference_between_largest_and_smallest_value_in_three_moves.ts

---
+++ 1509/minimum_difference_between_largest_and_smallest_value_in_three_moves.ts
@@ -0,0 +1,13 @@
+function minDifference(nums: number[]): number {
+  if (nums.length <= 4) {
+    return 0;
+  }
+
+  nums.sort((a, b) => a - b);
+  return Math.min(
+    nums[nums.length - 4] - nums[0],
+    nums[nums.length - 3] - nums[1],
+    nums[nums.length - 2] - nums[2],
+    nums[nums.length - 1] - nums[3],
+  );
+}
