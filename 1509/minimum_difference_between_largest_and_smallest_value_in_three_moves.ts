0ec787af052c0d980ffc138254c70b2f063d1f3e
EthanThatOneKid <31261035+EthanThatOneKid@users.noreply.github.com>
2024-07-03 09:53:31 -0700

    Update minimum_difference_between_largest_and_smallest_value_in_three_moves.ts

--- 1509/minimum_difference_between_largest_and_smallest_value_in_three_moves.ts
+++ 1509/minimum_difference_between_largest_and_smallest_value_in_three_moves.ts
@@ -1,13 +1,21 @@
 function minDifference(nums: number[]): number {
-  if (nums.length <= 4) {
-    return 0;
-  }
-
-  nums.sort((a, b) => a - b);
-  return Math.min(
-    nums[nums.length - 4] - nums[0],
-    nums[nums.length - 3] - nums[1],
-    nums[nums.length - 2] - nums[2],
-    nums[nums.length - 1] - nums[3],
+  return nums.length <= 4 ? 0 : Math.min(
+    ...nums.toSorted((a, b) => a - b)
+      .map((_, i, arr) => arr[arr.length - 4 + i] - arr[i])
+      .slice(0, 4),
   );
+}
+
+// Polyfill for Array.prototype.toSorted method.
+Array.prototype.toSorted = function <T>(
+  this: T[],
+  compareFn: (a: T, b: T) => number,
+): T[] {
+  return this.slice().sort(compareFn);
+};
+
+declare global {
+  interface Array<T> {
+    toSorted(compareFn: (a: T, b: T) => number): T[];
+  }
 }
