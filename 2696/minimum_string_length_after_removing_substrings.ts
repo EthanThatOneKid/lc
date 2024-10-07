f08221ceca1527a1e6bc233c81562909ce034384
Ethan Davidson <31261035+EthanThatOneKid@users.noreply.github.com>
2024-10-06 22:38:15 -0700

    Update minimum_string_length_after_removing_substrings.ts

--- 2696/minimum_string_length_after_removing_substrings.ts
+++ 2696/minimum_string_length_after_removing_substrings.ts
@@ -1,17 +1,12 @@
 function minLength(s: string): number {
-    return s.split("").reduce<string[]>((stack, c) => {
-        stack.push(c);
-        if (
+    return s.split("").reduce((prevStack, c, _, __, stack = prevStack + c) => ((
             stack.length >= 2 && (
                 (stack[stack.length - 2] === "A" &&
                     stack[stack.length - 1] === "B") ||
                 (stack[stack.length - 2] === "C" &&
                     stack[stack.length - 1] === "D")
             )
-        ) {
-            return stack.slice(0, -2);
-        }
-
-        return stack;
-    }, []).length;
+        )
+        ? stack.slice(0, -2)
+        : stack), "").length;
 }
