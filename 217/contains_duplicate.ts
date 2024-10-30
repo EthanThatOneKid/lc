101933658b737a52cf3da2f5c066019d20c1e677
Ethan Davidson <31261035+EthanThatOneKid@users.noreply.github.com>
2024-10-29 22:38:56 -0700

    Create contains_duplicate.ts

---
+++ 217/contains_duplicate.ts
@@ -0,0 +1,12 @@
+function containsDuplicate(nums: number[]): boolean {
+    const freaky = new Set<number>();
+    for (const n of nums) {
+        if (freaky.has(n)) {
+            return true;
+        }
+
+        freaky.add(n);
+    }
+
+    return false;
+}
