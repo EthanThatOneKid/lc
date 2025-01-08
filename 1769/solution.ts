f665456ef6a93e59ebb7d1aac6f1f19012e4b9d8
Ethan Davidson <31261035+EthanThatOneKid@users.noreply.github.com>
2025-01-08 10:36:19 -0800

    1769 from O(n^2) to O(n) B)

--- 1769/solution.ts
+++ 1769/solution.ts
@@ -1,31 +1,35 @@
 // deno 1769/solution.ts
 if (import.meta.main) {
-  console.log(simulate("001011"));
-  console.log(simulate("001011".split("").reverse().join("")).reverse());
+  // O(n) time complexity.
+  /** @see https://youtu.be/ZmH3gHiIqfI */
+  const example = "001011";
+  console.log(simulate(example));
+  console.log(simulate(example.split("").reverse().join("")).reverse());
+  console.log(minOperations(example));
 }
 
 function minOperations(boxes: string): number[] {
-  const a = simulate(boxes);
-  const b = simulate(boxes.split("").reverse().join("")).reverse();
-  return a.reduce((acc, _, i) => {
-    acc[i] = a[i] + b[i];
-    return acc;
-  }, Array.from({ length: boxes.length }, () => 0));
+  return simulate(
+    boxes,
+    simulate(boxes.split("").reverse().join("")).reverse(),
+  );
 }
 
-function simulate(boxes: string): number[] {
-  const result: number[] = Array.from({ length: boxes.length }, () => 0);
+function simulate(
+  boxes: string,
+  result = Array.from({ length: boxes.length }, () => 0),
+): number[] {
+  let balls = 0;
+  let moves = 0;
   for (let i = 0; i < boxes.length; i++) {
-    let moves = 0;
-    for (let j = i + 1; j < boxes.length; j++) {
-      if (boxes[j] === "0") {
-        continue;
-      }
-
-      moves += j - i;
+    const movesAt = balls + moves;
+    result[i] += movesAt;
+    moves = movesAt;
+    if (boxes[i] !== "1") {
+      continue;
     }
 
-    result[i] = moves;
+    balls++;
   }
 
   return result;
