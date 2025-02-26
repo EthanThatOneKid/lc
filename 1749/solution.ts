43c472e2db455b90f367e073508cb2af4acad318
Ethan Davidson <31261035+EthanThatOneKid@users.noreply.github.com>
2025-02-25 16:52:17 -0800

    Update solution.ts

--- 1749/solution.ts
+++ 1749/solution.ts
@@ -1,12 +1,11 @@
 function maxAbsoluteSum(nums: number[]): number {
-  let max = 0;
-  let min = 0;
-  let sum = 0;
-  for (const num of nums) {
-    sum += num;
-    max = Math.max(max, sum);
-    min = Math.min(min, sum);
-  }
-
-  return max - min;
+  return nums.reduce(
+    ({ max, min, sum }, num) => {
+      sum += num;
+      max = Math.max(max, sum);
+      min = Math.min(min, sum);
+      return { max, min, sum, absoluteSum: max - min };
+    },
+    { max: 0, min: 0, sum: 0, absoluteSum: 0 }
+  ).absoluteSum;
 }
