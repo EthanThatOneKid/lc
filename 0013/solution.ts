41f9a1f8a71d9bbac7dce4be676075c3c7b1a25f
Ethan Davidson <31261035+EthanThatOneKid@users.noreply.github.com>
2025-09-03 12:10:51 -0700

    Solve 13. Roman to Integer

---
+++ 0013/solution.ts
@@ -0,0 +1,64 @@
+const RomanSymbol = {
+  I: 1,
+  V: 5,
+  X: 10,
+  L: 50,
+  C: 100,
+  D: 500,
+  M: 1000,
+};
+
+function romanToInt(s: string): number {
+  let result = 0;
+  for (let i = 0; i < s.length; i++) {
+    const currentValue = RomanSymbol[s[i] as keyof typeof RomanSymbol];
+    const nextValue = RomanSymbol[s[i + 1] as keyof typeof RomanSymbol];
+
+    if (nextValue && currentValue < nextValue) {
+      result -= currentValue;
+    } else {
+      result += currentValue;
+    }
+  }
+
+  return result;
+}
+
+// Symbol       Value
+// I             1
+// V             5
+// X             10
+// L             50
+// C             100
+// D             500
+// M             1000
+
+// I can be placed before V (5) and X (10) to make 4 and 9.
+// X can be placed before L (50) and C (100) to make 40 and 90.
+// C can be placed before D (500) and M (1000) to make 400 and 900.
+// Given a roman numeral, convert it to an integer.
+
+// Example 1:
+
+// Input: s = "III"
+// Output: 3
+// Explanation: III = 3.
+// Example 2:
+
+// Input: s = "LVIII"
+// Output: 58
+// Explanation: L = 50, V= 5, III = 3.
+// Example 3:
+
+// Input: s = "MCMXCIV"
+// Output: 1994
+// Explanation: M = 1000, CM = 900, XC = 90 and IV = 4.
+
+// Constraints:
+
+// 1 <= s.length <= 15
+// s contains only the characters ('I', 'V', 'X', 'L', 'C', 'D', 'M').
+// It is guaranteed that s is a valid roman numeral in the range [1, 3999].
+
+// Seen this question in a real interview before?
+// 1/
